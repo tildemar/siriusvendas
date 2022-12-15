832b127346cd447b617bf8feed461eaae9907761
Tildemar Araujo <tildemar.araujo@gmail.com>
2022-12-15 13:02:00 -0300

    Primeiras Rotas

---
+++ app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrincipalController extends Controller
+{
+    public function principal() {
+        echo 'Iniciado o Sirius... Controller' ;
+    }
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return 'Iniciado o Sirius !!!' ; // 2022/12/15
+    // return view('welcome');
+});
+*/
+
+Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal']);
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre-nós' ; 
+});
+
+Route::get('/contato', function () {
+    return 'Contato' ; 
 });
